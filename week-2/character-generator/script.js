290ce13ec34243c55f340287a6fa08000b3e45ae
Robert King <raking45@hotmail.com>
2024-11-03 21:08:04 -0500

    Week 2 Completed

--- week-2/character-generator/script.js
+++ week-2/character-generator/script.js
@@ -3,8 +3,8 @@
   Chapter 1
   Programming Assignment
 
-  Author:
-  Date:
+  Author: Robert King
+  Date:   November 3, 2024
   Filename:
 */
 
@@ -12,14 +12,41 @@
 
 function createCharacter(name, gender, characterClass) {
   // TODO: Implement this function
+  return {
+    getName: function () {
+      return name;
+    },
+    getGender: function () {
+      return gender;
+    },
+    getClass: function () {
+      return characterClass;
+    }
+  };
 }
 
 document.getElementById("generateHero").addEventListener("click", function(e) {
   e.preventDefault();
 
   // TODO: Get form values
+  const name = document.getElementById("heroName").value;
+  const gender = document.getElementById("heroGender").value;
+  const characterClass = document.getElementById("heroClass").value;
+
+  // Form field entry validation
+  if (!name || !gender || !characterClass) {
+    alert("Please fill out all fields.");
+    return;
+  }
 
   // TODO: Create character
+  const character = createCharacter(name, gender, characterClass);
 
   // TODO: Display character information
+  const characterOutput = document.getElementById("characterOutput");
+  characterOutput.innerHTML = `
+  <p><strong>Name:</strong><b>${character.getName()}</b></p>
+  <p><strong>Gender:</strong><b>${character.getGender()}</b></p>
+  <p><strong>Class:</strong><b>${character.getClass()}</b></p>
+  `;
 });
